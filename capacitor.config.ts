1742fd86527595eee8ca73122d49e14ade3bc86f
regtat <regitaghani@gmail.com>
2024-11-13 15:15:26 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vue-firebase-prak-pemob',
+  webDir: 'dist'
+};
+
+export default config;
